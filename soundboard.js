a670d092b817c9a074ded98a214e662c7f40263d
Tom Burridge <tjburridge@gmail.com>
2019-07-18 21:42:36 -0700

    Added audio files covering all current episodes of Dialed up to ep26.
    Minor changes to page layout.

--- soundboard.js
+++ soundboard.js
@@ -19,6 +19,18 @@ var soundData = [
     },
     {
         sound: new Howl({
+            src: ['sounds/bah4.mp3']
+        }),
+        description: 'Bah 4'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/bahbah.mp3']
+        }),
+        description: 'Bahbah'
+    },
+    {
+        sound: new Howl({
             src: ['sounds/coow.mp3']
         }),
         description: 'Coow'
@@ -43,9 +55,33 @@ var soundData = [
     },
     {
         sound: new Howl({
+            src: ['sounds/raspberry3.mp3']
+        }),
+        description: 'Raspberry 3'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/raspberry4.mp3']
+        }),
+        description: 'Raspberry 4'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/raspberry5.mp3']
+        }),
+        description: 'Raspberry 5'
+    },
+    {
+        sound: new Howl({
             src: ['sounds/waananan.mp3']
         }),
         description: 'Waananan'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/waananan3.mp3']
+        }),
+        description: 'Waananan 2'
     },
     {
         sound: new Howl({
@@ -76,6 +112,138 @@ var soundData = [
             src: ['sounds/aaagh.mp3']
         }),
         description: 'Aaagh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/arnie.mp3']
+        }),
+        description: 'Arnie'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/baambaambaam.mp3']
+        }),
+        description: 'Baam baam baam'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/cahcahcah.mp3']
+        }),
+        description: 'Cah cah cah'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/cahcahcahcahcah.mp3']
+        }),
+        description: 'Cahcahcahcahcah'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/duusvduusvduusv.mp3']
+        }),
+        description: 'Duusvduusvduusv'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/eeegh.mp3']
+        }),
+        description: 'Eeegh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/Eeey.mp3']
+        }),
+        description: 'Eeey'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/eysh.mp3']
+        }),
+        description: 'Eysh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/fshhhh.mp3']
+        }),
+        description: 'Fshhhh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/kissing.mp3']
+        }),
+        description: 'Kissing'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/kuhkuuh.mp3']
+        }),
+        description: 'Kuhkuuh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/kuuh.mp3']
+        }),
+        description: 'Kuuh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/Kuuung.mp3']
+        }),
+        description: 'Kuuung'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/snacking.mp3']
+        }),
+        description: 'Snacking'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/tweettweet.mp3']
+        }),
+        description: 'Tweet tweet'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/vraah.mp3']
+        }),
+        description: 'Vraah'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/Vuuush.mp3']
+        }),
+        description: 'Vuuush'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/Waah.mp3']
+        }),
+        description: 'Waah'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/wahwaaaaaaaah.mp3']
+        }),
+        description: 'Wahwaaaaaaaah'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/wehdehdeh.mp3']
+        }),
+        description: 'Wehdehdeh'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/wuuoooow.mp3']
+        }),
+        description: 'Wuuoooow'
+    },
+    {
+        sound: new Howl({
+            src: ['sounds/yeeeenup.mp3']
+        }),
+        description: 'Yeeeenup'
     },
 ]
 
